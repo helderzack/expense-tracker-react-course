febd134ba43f7728535828879d715c8eb9f5ef14
helderzack <helderzacarias@outlook.com>
2022-02-01 18:28:23 -0300

    Start of the implementation of the chart area

--- src/components/ExpenseChart/ExpenseChart.js
+++ src/components/ExpenseChart/ExpenseChart.js
@@ -2,8 +2,51 @@ import './ExpenseChart.css';
 
 function ExpenseChart() {
   return(
-    <div>
-      <p>Expense Chart</p>
+    <div className="chart-area">
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Jan</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Fev</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Mar</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Apr</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>May</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Jun</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Jul</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Aug</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Sep</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Nov</p>
+      </div>
+      <div className='chart-item'>
+        <div className='chart-draw'></div>
+        <p>Dec</p>
+      </div>
     </div>
   );
 }
